a777037502e51ea3a8b20bf019d115acf194e2f9
igarashi <igarashi.kouki@gmail.com>
2020-12-02 14:54:07 +0900

    day 1

---
+++ 1/a.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	// O(N)
+
+	bucket := [2021]bool{}
+	for {
+		var expense int
+		_, err := fmt.Scan(&expense)
+		if err != nil {
+			log.Print(err)
+			break
+		}
+		if expense < 0 {
+			log.Print("Negative expense.")
+			// break
+			continue
+		}
+		if expense > 2020 {
+			continue
+		}
+		if bucket[2020 - expense] {
+			fmt.Println((2020 - expense) * expense)
+		}
+		bucket[expense] = true
+	}
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-advent-of-code-2020
+# Advent of code 2020
+
+https://adventofcode.com/
+
