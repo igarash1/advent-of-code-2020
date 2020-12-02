420f2d00d01170b1a77acc4ccacd1e1cb5ad3771
igarashi <igarashi.kouki@gmail.com>
2020-12-02 22:28:38 +0900

    day 2

---
+++ 2/a.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// O(N * L), where L is the maximum length of the password.
+
+	result := 0
+	for {
+		var lo, hi int
+		var letter byte
+		var pw string
+		
+		_, err := fmt.Scanf("%d-%d %c: %s", &lo, &hi, &letter, &pw)
+		
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		cnt := 0
+		for _, c := range pw {
+			if byte(c) == letter {
+				cnt++
+			}
+		}
+		if lo <= cnt && cnt <= hi {
+			result++
+		}
+	}
+
+	fmt.Println(result)
+}
+
---
+++ 2/b.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// O(N)
+	
+	result := 0
+	for {
+		var p1, p2 int
+		var letter byte
+		var pw string
+		
+		_, err := fmt.Scanf("%d-%d %c: %s", &p1, &p2, &letter, &pw)
+		
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		cnt := 0
+		if 0 < p1 && p1 <= len(pw) && byte(pw[p1 - 1]) == letter {
+			cnt++
+		}
+		if 0 < p2 && p2 <= len(pw) && byte(pw[p2 - 1]) == letter {
+			cnt++
+		}
+
+		if cnt == 1 {
+			result++
+		}
+	}
+
+	fmt.Println(result)
+}
+
